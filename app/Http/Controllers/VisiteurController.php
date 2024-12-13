303b6dd6ae676b176384a4c11c1df35c4407e766
MarieJo GARET <mjogaret@yahoo.fr>
2024-12-13 16:38:21 +0100

    add  modifier controller

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -12,49 +12,45 @@ class VisiteurController extends Controller
    *
    * @return \Illuminate\Contracts\View\View
    */
-  public function layout() {
-    return view('/layout'); //Affiche la vue accueil.blade.php présent le dossier ressources/view
-  }
-  /**
-   * Affichage de la page accueil
-   *
-   * @return \Illuminate\Contracts\View\View
-   */
   public function accueil() {
-    return view('/accueil'); //Affiche la vue accueil.blade.php présent le dossier ressources/view
+    return view('accueil'); //Affiche la vue accueil.blade.php présent le dossier ressources/view
   }
+
   /**
-   * Affichage de la page navigation
-   *
-   * @return \Illuminate\Contracts\View\View
-   */
-  public function navigation() {
-    return view('/navigation'); //Affiche la vue navigation.blade.php présent le dossier ressources/view
-  }
-/**
    * Affichage de la page de présentation
    *
    * @return \Illuminate\Contracts\View\View
    */
-  public function presentation() {
-    return view('/presentation');
+
+  public function formation() {
+    return view('formation');
   }
+
   /**
-   * Affichage de la page de la formation mf1
+   * Affichage de la page information MF1
    *
    * @return \Illuminate\Contracts\View\View
    */
   public function mf1() {
-    return view('/mf1');
+    return view('mf1');
   }
 
   /**
-   * Affichage de la page de formation MF2
+   * Affichage de la page information MF2
    *
    * @return \Illuminate\Contracts\View\View
    */
   public function mf2() {
-    return view('/mf2');
+    return view('mf2');
+  }
+
+  /**
+   * Affichage de la page information Niveau 4 Guide de Palanquée
+   *
+   * @return \Illuminate\Contracts\View\View
+   */
+  public function niveau4() {
+    return view('niveau4');
   }
 
   /**
@@ -63,7 +59,7 @@ class VisiteurController extends Controller
    * @return \Illuminate\Contracts\View\View
    */
   public function contact() {
-    return view('/contact');
+    return view('contact');
   }
 
   /**
@@ -72,6 +68,6 @@ class VisiteurController extends Controller
    * @return \Illuminate\Contracts\View\View
    */
   public function mentionslegales() {
-    return view('/mentionslegales');
+    return view('mentionslegales');
   }
 }
