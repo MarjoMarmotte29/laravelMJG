e0a80724ab5dc46c6687e25b2b0f34ee3d21af0d
MarieJo GARET <mjogaret@yahoo.fr>
2024-12-13 16:48:15 +0100

    news

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use App\Http\Controllers\VisiteurController;
+use Illuminate\Support\Facades\Route;
 
 //Route pour la partie visiteur - utilisation d'un controlleur
 Route::controller(VisiteurController::class)->group(function () {
-  Route::get('/', "layout")->name("layout"); //Exécute la fonction accueil présent dans VisiteurController
-  Route::get('/navigation', "navigation")->name("navigation");
-  Route::get('/presentation', "presentation")->name("presentation");
-  Route::get('/mf1', "mf1")->name("Formation MF1");
-  Route::get('/mf2', "mf2")->name("Formation MF2");
+  Route::get('/', "accueil")->name("accueil"); //Exécute la fonction accueil présent dans VisiteurController
+  Route::get('/formation', "formation")->name("formation");
+  Route::get('/mf1', "mf1")->name("mf1");
+  Route::get('/mf2', "mf2")->name("mf2");
+  Route::get('/niveau4', "niveau4")->name("niveau4");
   Route::get('/contact', "contact")->name("contact");
   Route::get('/mentionslegales', "mentionslegales")->name("mentionslegales");
 });
-
