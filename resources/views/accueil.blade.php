f77290bab4ad9ba7b82c262aa7d9a1505c1b23d0
MarieJo GARET <mjogaret@yahoo.fr>
2024-12-14 18:25:55 +0100

    accueil ?

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,7 +5,7 @@
 
 <div>
    <p>Ici il y aura le carousel Photos</p>
-   <p>Ici il y aura le Loigo</p>
+   <p>Ici il y aura le Logo</p>
    <p>Ici il y aura le contact et lien s sociaux</p>
 </div>
 <div class="d-flex flex-row mb-3 d-flex justify-content-end">
