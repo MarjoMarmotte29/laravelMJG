aee06c8d4572d440a0bff78a95fffab2ea8e305b
MarieJo GARET <mjogaret@yahoo.fr>
2024-12-14 17:46:03 +0100

    page mf2

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,17 +2,13 @@
 @section("titre", "Gwenrann Formation")
 
 @section("contenu")
-  
 
 <div>
    <p>Ici il y aura le carousel Photos</p>
+   <p>Ici il y aura le Loigo</p>
+   <p>Ici il y aura le contact et lien s sociaux</p>
 </div>
 <div class="d-flex flex-row mb-3 d-flex justify-content-end">
    @include('formation')
 </div>
-<div class="col-6">
-  <h1> Contact</h1>
-    @yield('contact')
-</div>
-
 @endsection
--- resources/views/mf2.blade.php
+++ resources/views/mf2.blade.php
@@ -2,17 +2,36 @@
 @section("titre", "Formation MF2")
 
 @section("contenu")
-<div> 
-            <p>Dans chaque MF1, il y a un MF2 qui sommeille.</p>
-            <p>Devenir formateur de cadre, l' équipe de bénévoles Gwenrann Formation vous aide toute l'année à atteindre cet objectif ! Notre devise :</p >
-            <p>Notre devise : </p>
-            <li>plonger, </li>
-            <li>s'entraîner, </li>
-            <li>réviser dans la joie et la bonne humeur!</li>
-            <p> Venez nous rejoindre</p>
- 
-  
+<div>
+  <h1><b>Fromation MF2</b></h1>
+  <p>Gwenrann-formation organise un week-end par mois d'entrainement physique, théorique et pédagogique pour enrichir vos compétences, durant le stage en situation, formation adaptée aux nouvelles épreuves MF2. Voici en quelques lignes, le déroulé de nos séances :</p> 
+    <div class="d-flex justify-content-around">
+      <div class="d-flex flex-row mb-3">
+        <div class="p-2">
+          <h2>Entrainement Physique</h2>
+          <p>Les différentes épreuves : </p>
+          <li>1500 pmt ou 1000 capelé</li>
+          <li>L'apnée à 15 m</li>
+          <li>Le mannequin : 200 m nage, apnée 10m, 100 m tractage</li>
+          <li>Les épreuves de plongée à 50m</li>
+          <li>La plongée à 35m : DTMR</li>
+          <img class="img-rounded" src="/images/accueil_d.png">
+        </div>
+        <div class="p-2">
+          <h2>Théorie</h2>
+          <p>Sous forme de Questions à Réponses Ouvertes et Courtes, les fameuses QROCS permettent aux stagiaires de réviser en continu. Chaque week-end est l'occasion d'aborder 3 thèmes.</p>
+          <p> Avec la mise en place de la plate-forme Moodle, les créateurs de cours élaborent des QCM facilitant la démarche de révision</p>
+          <img class="img-rounded" src="/images/08.jpg">
+        </div>
+        <div class="p-2">
+          <h2>Pédagogie</h2>
+          <p>Les mois d’hiver, quelques WE sont consacrés exclusivement à la pédagogie. Ces WE sont destinés aussi bien aux stagiaires MF1 qu’aux Stagiaires MF2. La péda n’est pas oubliée pendant les autres WE, qu’il s’agisse de péda théorique, pratique ou organisationnelle.</p>
+          <img class="img-rounded" src="/images/F1_F2.jpg">
+        </div>
+      </div>
+    </div>
 
 Venez nous rejoindre ! 
-  <img src="/images/accueil_g.png" alt="image carrière"></a>
+<a href="#"> retour en haut</a>
+</div>
 @endsection
